fd6ee55c99ff034d576680ba2de99c4a365be634
WElligton Rios <welligtonrios2@gmail.com>
2024-07-25 08:56:05 -0300

    fetching comments

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 import Head from "next/head";
 import  styles  from "./style.module.css";
 import { GetServerSideProps } from "next";
-import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
 import { db } from "@/service/firebaseConnection";
 import { Textarea } from "@/components/textarea";
 
@@ -15,14 +15,24 @@ interface TaskProps {
         created: string,
         user:  string,
         taskId:  string
-    }
+    },
+    allComments : CommentProps[];
 }
 
-export default function Task({ item }: TaskProps) {
+interface CommentProps {
+    id: string;
+    comment: string;
+    taskId: string;
+    user: string;
+    name: string;
+  }
+
+export default function Task({ item, allComments }: TaskProps) {
 
     const { data: session } = useSession();
 
     const [input, setInput] = useState("");
+    const [comments, setComments] = useState<CommentProps[]>(allComments || []);
 
     async function handleComment(event: FormEvent) {
         event.preventDefault();
@@ -48,32 +58,45 @@ export default function Task({ item }: TaskProps) {
 
     return (
       <div className={styles.container}>
-        <Head>
-          <title>Tarefa - Detalhes da tarefa</title>
-        </Head>
+            <Head>
+            <title>Tarefa - Detalhes da tarefa</title>
+            </Head>
   
-        <main className={styles.main}>
-          <h1>Tarefa</h1>
-          <article className={styles.task}>
-            <p>{item.tarefa}</p>
-          </article>
-        </main>
+            <main className={styles.main}>
+                <h1>Tarefa</h1>
+                <article className={styles.task}>
+                    <p>{item.tarefa}</p>
+                </article>
+            </main>
   
-        <section className={styles.commentsContainer}>
-          <h2>Deixar comentário</h2>
-  
-          <form onSubmit={handleComment}>
-            <Textarea 
-            value={input}
-            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-                setInput(event.target.value)}
-            placeholder="Digite seu comentário..." 
-            />
-            <button className={styles.button} 
-                    disabled={!session?.user} >
-                Enviar comentário
-            </button>
-          </form>
+            <section className={styles.commentsContainer}>
+                <h2>Deixar comentário</h2>
+    
+                <form onSubmit={handleComment}>
+                    <Textarea 
+                    value={input}
+                    onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+                        setInput(event.target.value)}
+                    placeholder="Digite seu comentário..." 
+                    />
+                    <button className={styles.button} 
+                            disabled={!session?.user} >
+                        Enviar comentário
+                    </button>
+                </form>
+            </section>
+
+            <section className={styles.commentsContainer}>
+                <h2>Todos comentários</h2>
+                {comments.length === 0 && (
+                <span>Nenhum comentário foi encontrado...</span>
+                )}
+
+                {comments.map((item) => (
+                <article key={item.id} className={styles.comment}>
+                    <p>{item.comment}</p>
+                </article>
+                ))}
         </section>
       </div>
     );
@@ -84,6 +107,23 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     const id = params?.id as string;
     
     const docRef = doc(db, "tarefas", id);
+    
+
+    const q = query(collection(db, "comments"), where("taskId", "==", id));
+    const snapshotComments = await getDocs(q);
+
+    let allComments: CommentProps[] = [];
+    snapshotComments.forEach((doc) => {
+        allComments.push({
+        id: doc.id,
+        comment: doc.data().comment,
+        user: doc.data().user,
+        name: doc.data().name,
+        taskId: doc.data().taskId,
+        });
+    });
+
+
     const snapshot = await getDoc(docRef);
 
     if (snapshot.data() === undefined || !snapshot.data()?.public) {
@@ -107,7 +147,8 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
    
     return {
         props: {
-            item: task
+            item: task,
+            allComments : allComments
         },
       };
 }
